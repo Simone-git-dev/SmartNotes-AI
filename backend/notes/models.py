2a408b976d5655a00fb040e3af45a735c38d4ba2
MouzeeSrl <118256490+MouzeeSrl@users.noreply.github.com>
2026-04-23 12:29:25 +0200

    feat: add Note model

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    resume_ai = models.TextField(blank=True, null=True)
+        
