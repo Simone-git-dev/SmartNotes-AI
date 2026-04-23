b4acf74372c62fde0e1ee467067970dd03430186
MouzeeSrl <118256490+MouzeeSrl@users.noreply.github.com>
2026-04-23 17:39:10 +0200

    feat: add JWT authentication and user registration

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('notes.urls'))
+    path('api/', include('notes.urls')),
+    path('api/auth/', include('users.urls')),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+
+class RegisterView(APIView):
+    
+    permission_classes = [AllowAny]  # ← pubblico!
+
+    def post(self, request):
+        
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        # 1. Controlla se l'utente esiste già
+        if User.objects.filter(username=username).exists():
+            return Response({'error': "User already exists"}, status = status.HTTP_400_BAD_REQUEST)
+        else:
+            # 2. Crea l'utente
+            user = User.objects.create_user(username=username, email=email, password=password)
+
+        refresh = RefreshToken.for_user(user)
+        
+        # 4. Restituisci i token
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
